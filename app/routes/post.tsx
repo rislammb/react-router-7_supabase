b00ca9d1f32686f08bbacc4722648285edab2e9a
Rayhanul Islam <rislammb@gmail.com>
2025-04-14 17:55:16 +0600

    added dynamic data fetching

--- app/routes/post.tsx
+++ app/routes/post.tsx
@@ -1,10 +1,17 @@
 import type { Route } from "./+types/post";
 
-export async function loader({ params }: Route.LoaderArgs) {
-  const postId = params.postId;
-  return { postId };
+export async function clientLoader({ params }: Route.LoaderArgs) {
+  const posts = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.postId}`
+  );
+  return await posts.json();
 }
 
 export default function Post({ loaderData }: Route.ComponentProps) {
-  return <div>Post ID: {loaderData.postId}</div>;
+  return (
+    <div>
+      <h2>Title: {loaderData.title}</h2>
+      <p>{loaderData.body}</p>
+    </div>
+  );
 }
