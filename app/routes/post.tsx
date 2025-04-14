3709a5c1b9c67342113f9e0a9e4c97f24e74f7ff
Rayhanul Islam <rislammb@gmail.com>
2025-04-14 17:55:59 +0600

    added data delete action

--- app/routes/post.tsx
+++ app/routes/post.tsx
@@ -1,3 +1,4 @@
+import { Form, redirect } from "react-router";
 import type { Route } from "./+types/post";
 
 export async function clientLoader({ params }: Route.LoaderArgs) {
@@ -7,11 +8,23 @@ export async function clientLoader({ params }: Route.LoaderArgs) {
   return await posts.json();
 }
 
+export async function clientAction({ params }: Route.LoaderArgs) {
+  await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`, {
+    method: "DELETE",
+  });
+
+  return redirect("/");
+}
+
 export default function Post({ loaderData }: Route.ComponentProps) {
   return (
     <div>
       <h2>Title: {loaderData.title}</h2>
       <p>{loaderData.body}</p>
+
+      <Form method="delete">
+        <button type="submit">Delete</button>
+      </Form>
     </div>
   );
 }
